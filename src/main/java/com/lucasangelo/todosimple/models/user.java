727c4879f03b7ff94bab7ba5c1f4d81f34c76bd0
Gabriely <gabrielyvieira03@gmail.com>
2025-10-01 19:05:26 -0300

    Create User and Task repositories

--- src/main/java/com/lucasangelo/todosimple/models/user.java
+++ src/main/java/com/lucasangelo/todosimple/models/user.java
@@ -1,63 +1,48 @@
 package com.lucasangelo.todosimple.models;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import javax.persistence.CollectionTable;
+import java.util.List;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
-import com.lucasangelo.todosimple.models.enums.ProfileEnum;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "User.TABLE_NAME")
-
 public class User {
-    public interface CreateUser {
-    }
-
-    public interface UpdateUser {
-    }
+    public interface CreateUser {}
+    public interface UpdateUser {}
 
     public static final String TABLE_NAME = "user";
 
     @Id
-    @GeneratedValue(strategy = GeneretionType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", lenght = 100, nullable = false, unique = true)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 2, max = 200)
     private String username;
 
-@JsonProperty(access = Access.WRITE_ONLY)
-    @Column(name = "password", lenght = 60, nullable = false)
+    @Column(name = "password", length = 60, nullable = false)
     @NotNull(groups = { CreateUser.class, UpdateUser.class })
     @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task> ();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task> ();
+
+
 
     public User() {
     }
@@ -91,5 +76,49 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public User id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public User username(String username) {
+        setUsername(username);
+        return this;
+    }
+
+    public User password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (obj == null)
+        return false;
+        if (!(obj instanceof User)) 
+            return false;
+        User other = (User) obj;
+
+        if (this.id == null)   
+        if (other.id != null)
+        return false; 
+        else if (!this.id.equals(other.id))
+        return false;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.username, other.username)
+        && Objects.equals(this.password, other.password);
+        
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+
 
 }
