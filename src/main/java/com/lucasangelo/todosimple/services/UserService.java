76a2092c2057e85b42c23a97b682d737679f7fe7
Gabriely <gabrielyvieira03@gmail.com>
2025-10-01 20:02:21 -0300

    Create Task Service

--- src/main/java/com/lucasangelo/todosimple/services/UserService.java
+++ src/main/java/com/lucasangelo/todosimple/services/UserService.java
@@ -18,9 +18,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
         
